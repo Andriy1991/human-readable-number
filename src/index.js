7e09ef49f2689a6521a43726a41a4ccf20b7f0ba
Andriy1991 <andriy-5199029@mail.ru>
2021-03-16 02:56:37 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function toReadable (number) {
+  let first = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+  let second = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety']
   
+  if (number === 0) return 'zero'
+  if (number < 20) return first[number]
+
+  let dig = number % 10
+  if (number < 100) {
+    return second[Math.floor(number / 10)] + (number % 10 === 0 ? '' : ' ' + first[dig] )
+  }
+  if (number < 1000) {
+    return first[Math.floor(number / 100)] + ' hundred' + (number % 100 === 0 ? '' : ' ' + toReadable(number % 100))
+  }
 }
